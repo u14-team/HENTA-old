10fc07b32c88bc666fb2a6a326aea89298983a6d
TheEVolk <elektro-volk@yandex.ru>
2020-01-07 14:24:08 +0000

    feat: pickRandomIndex и проверка абсолютного пути под windows

--- src/util.js
+++ src/util.js
@@ -1,19 +1,33 @@
 import { promises as fs } from 'fs';
+import path from 'path';
 
 export default class Util {
   constructor(henta) {
     this.henta = henta;
   }
 
-  async loadSettings(path) {
+  async loadSettings(settingsPath) {
     // TODO: windows
-    const fullPath = path.startsWith('/') ? path : `${this.henta.botdir}/settings/${path}`;
+    const fullPath = path.isAbsolute(settingsPath)
+      ? settingsPath
+      : `${this.henta.botdir}/settings/${settingsPath}`;
+
     const data = await fs.readFile(fullPath);
     return JSON.parse(data);
   }
 
+  /** Осторожно, название этой функции может меняться. */
+  async loadEnts(entsPath) {
+    const data = await this.loadSettings(entsPath);
+    return [data, this.createFromSlug(data)];
+  }
+
   pickRandom(array) {
     return array[Math.floor(Math.random() * array.length)];
+  }
+
+  pickRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
   }
 
   createFromSlug(array) {
